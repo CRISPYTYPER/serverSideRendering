89e613fa69acbab125dcfc5991ed0836c40f6283
Lee-Jiin <jiinlee24@naver.com>
2022-04-20 11:19:51 +0900

    page 576

--- src/index.server.js
+++ src/index.server.js
@@ -4,6 +4,11 @@ import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
 import fs from 'fs';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './modules';
+import PreloadContext from './lib/PreloadContext';
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -15,7 +20,7 @@ const chunks = Object.keys(manifest.files)
     .map((key) => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
     .join(''); //합침
 
-function createPage(root) {
+function createPage(root, stateScript) {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -27,16 +32,17 @@ function createPage(root) {
         />
         <meta name="theme-color" content="#000000" />
         <title>React App</title>
-        <link href="${manifest.files['main.css']}" rel="stylesheet" />
+        <link href="${manifest['main.css']}" rel="stylesheet" />
     </head>
     <body>
         <noscript>You need to enable JavaScript to run this app.</noscript>
         <div id="root">
             ${root}
         </div>
-        <script src="${manifest.files['runtime-main.js']}"></script>
+        ${stateScript}
+        <script src="${manifest['runtime-main.js']}"></script>
         ${chunks}
-        <script src="${manifest.files['main.js']}"></script>
+        <script src="${manifest['main.js']}"></script>
     </body>
     </html>
     `;
@@ -45,16 +51,39 @@ function createPage(root) {
 const app = express();
 
 // 서버 사이드 렌더링을 처리할 핸들러 함수입니다.
-const serverRender = (req, res, next) => {
+const serverRender = async (req, res, next) => {
     // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해줍니다.
     const context = {};
+    const store = createStore(rootReducer, applyMiddleware(thunk));
+
+    const preloadContext = {
+        done: false,
+        promises: [],
+    };
     const jsx = (
-        <StaticRouter location={req.url} context={context}>
-            <App />
-        </StaticRouter>
+        <PreloadContext.Provider value={preloadContext}>
+            <Provider store={store}>
+                <StaticRouter location={req.url} context={context}>
+                    <App />
+                </StaticRouter>
+            </Provider>
+        </PreloadContext.Provider>
     );
+
+    ReactDOMServer.renderToStaticMarkup(jsx);
+    try {
+        await Promise.all(preloadContext.promises); //모든 프로미스를 기다립니다.
+    } catch (e) {
+        return res.status(500);
+    }
+    preloadContext.done = true;
     const root = ReactDOMServer.renderToString(jsx);
-    res.send(createPage(root));
+    const stateString = JSON.stringify(store.getState()).replace(
+        /</g,
+        '\\u003c',
+    );
+    const stateScript = `<script>__PRELOADED_STATE__=${stateString}</script>`; //리덕스 초기 상태를 스크립트로 주입
+    res.send(createPage(root, stateScript)); // 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve('./build'), {
